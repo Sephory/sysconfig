cd634686a86d2a0b3f9503d53375ae1fcef14d44
Brian <brian@bteksoftware.com>
2021-03-29 08:53:24 -0700

    don't install plugins

--- arch/base.sh
+++ arch/base.sh
@@ -40,7 +40,3 @@ chezmoi apply
 curl -Lo  ~/.zsh/antigen.zsh --create-dirs git.io/antigen
 git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-#Install Plugins
-~/.tmux/plugins/tpm/bin/install_plugins
-nvim --headless +PackerInstall +qa
