f9455721bd3e95e25b195b97110fd6429d97dc5f
Brian <brian@bteksoftware.com>
2021-03-29 08:44:03 -0700

    export variables

--- arch/base.sh
+++ arch/base.sh
@@ -29,9 +29,9 @@ rm -R /tmp/neovim-build
 
 #Get dotfiles and apply
 USERNAME=${$1:-sephory}
-HOME=${$2:-/home/$USERNAME}
+export HOME=${$2:-/home/$USERNAME}
 cd $HOME
-BW_SESSION=$(bw login $BW_USERNAME $BW_PASSWORD --raw)
+export BW_SESSION=$(bw login $BW_USERNAME $BW_PASSWORD --raw)
 unset BW_USERNAME BW_PASSWORD
 chezmoi init https://$(bw get username github.com):$(bw get password github.com)@github.com/sephory/dotfiles
 chezmoi apply
